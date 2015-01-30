c2c77b7db1e7f19f320a1f74c063ffe95a5c7c6d
Ali Moghnieh <SupaHam.me@gmail.com>
2015-01-30 08:30:02 +0000

    Added PlaceholderFunction parameter to all constructors.

--- commons-core/src/main/java/com/supaham/commons/placeholders/PlaceholderSet.java
+++ commons-core/src/main/java/com/supaham/commons/placeholders/PlaceholderSet.java
@@ -1,11 +1,13 @@
 package com.supaham.commons.placeholders;
 
+import static com.google.common.base.Preconditions.checkNotNull;
 import static com.supaham.commons.utils.StringUtils.checkNotNullOrEmpty;
 
 import java.util.Collection;
 import java.util.HashSet;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 /**
  * Represents a {@link HashSet} of {@link Placeholder}s. This class also provides {@link
@@ -24,27 +26,72 @@ public class PlaceholderSet<T extends Placeholder> extends HashSet<T> {
    * {@inheritDoc}
    */
   public PlaceholderSet() {
+    super();
+  }
+
+  /**
+   *
+   */
+  public PlaceholderSet(@Nonnull PlaceholderFunction function) {
+    super();
+    checkNotNull(function);
+    this.placeholderFunction = function;
   }
 
   /**
    * {@inheritDoc}
    */
   public PlaceholderSet(Collection<T> c) {
+    this(c, null);
+  }
+
+  /**
+   * @param function function to handle placeholders. If null, the default is used instead
+   *
+   * @see #PlaceholderSet(Collection)
+   */
+  public PlaceholderSet(Collection<T> c, @Nullable PlaceholderFunction function) {
     super(c);
+    if (function != null) {
+      this.placeholderFunction = function;
+    }
   }
 
   /**
    * {@inheritDoc}
    */
   public PlaceholderSet(int initialCapacity, float loadFactor) {
+    this(initialCapacity, loadFactor, null);
+  }
+
+  /**
+   * @param function function to handle placeholders. If null, the default is used instead
+   * @see #PlaceholderSet(int, float) 
+   */
+  public PlaceholderSet(int initialCapacity, float loadFactor,
+                        @Nullable PlaceholderFunction function) {
     super(initialCapacity, loadFactor);
+    if (function != null) {
+      this.placeholderFunction = function;
+    }
   }
 
   /**
    * {@inheritDoc}
    */
   public PlaceholderSet(int initialCapacity) {
+    this(initialCapacity, null);
+  }
+
+  /**
+   * @param function function to handle placeholders. If null, the default is used instead
+   * @see #PlaceholderSet(int)
+   */
+  public PlaceholderSet(int initialCapacity, @Nullable PlaceholderFunction function) {
     super(initialCapacity);
+    if (function != null) {
+      this.placeholderFunction = function;
+    }
   }
 
   /**
@@ -60,5 +107,24 @@ public class PlaceholderSet<T extends Placeholder> extends HashSet<T> {
   @Nonnull
   public String apply(String input) {
     return placeholderFunction.apply(checkNotNullOrEmpty(input));
+  }
+
+  /**
+   * Gets the {@link PlaceholderFunction} of this {@link PlaceholderSet}.
+   *
+   * @return placeholder function
+   */
+  public PlaceholderFunction getPlaceholderFunction() {
+    return placeholderFunction;
+  }
+
+  /**
+   * Sets the {@link PlaceholderFunction} of this {@link PlaceholderSet} for overriding the
+   * default.
+   *
+   * @param function placeholder function
+   */
+  public void setPlaceholderFunction(PlaceholderFunction function) {
+    this.placeholderFunction = function;
   }
 }
