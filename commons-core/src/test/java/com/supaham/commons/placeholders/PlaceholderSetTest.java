d0a11f7040a5fb85b97e219990cbf5f4aa8f5d7f
Ali Moghnieh <SupaHam.me@gmail.com>
2015-01-30 08:24:18 +0000

    Added a few more Placeholder tests.

--- commons-core/src/test/java/com/supaham/commons/placeholders/PlaceholderSetTest.java
+++ commons-core/src/test/java/com/supaham/commons/placeholders/PlaceholderSetTest.java
@@ -4,20 +4,40 @@ import static com.supaham.commons.placeholders.SimplePlaceholderTest.PDNAME;
 import static com.supaham.commons.placeholders.SimplePlaceholderTest.PNAME;
 import static com.supaham.commons.placeholders.SimplePlaceholderTest.SIMPLE_PLACEHOLDER;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Created by Ali on 28/01/2015.
  */
 public class PlaceholderSetTest {
+  
+  PlaceholderSet<Placeholder> set = new PlaceholderSet<>();
+  
+  @Before
+  public void before() {
+    set.add(SIMPLE_PLACEHOLDER);
+  }
 
   @Test
   public void testPlaceholderFunction() {
-    PlaceholderSet<Placeholder> set = new PlaceholderSet<>();
-    set.add(SIMPLE_PLACEHOLDER);
     String input = "Hi, my IGN is {pname}. However, my display name is {pdname}.";
     String expected = input.replace("{pname}", PNAME).replace("{pdname}", PDNAME);
     assertEquals(expected, set.apply(input));
+  }
+  
+  @Test
+  public void testContains() {
+    assertTrue(set.contains(SIMPLE_PLACEHOLDER));
+  }
+  
+  @Test
+  public void testRemove() {
+    PlaceholderSet<Placeholder> set = new PlaceholderSet<>();
+    set.remove(SIMPLE_PLACEHOLDER);
+    assertFalse(set.contains(SIMPLE_PLACEHOLDER));
   }
 }
