2847d694d06dee9b42559b496dd18e5c0c517b7b
Ali Moghnieh <SupaHam.me@gmail.com>
2015-02-07 06:19:01 +0000

    Created getRandomElement(Collection<Object>) and added delegate method for ArrayUtils.getRandomElement(Object[]) in CollectionUtils.

--- commons-core/src/main/java/com/supaham/commons/utils/CollectionUtils.java
+++ commons-core/src/main/java/com/supaham/commons/utils/CollectionUtils.java
@@ -4,23 +4,42 @@ import java.util.Collection;
 import java.util.List;
 
 /**
- * Utility methods for working with {@link Collection}s. This class contains methods such as
- * {@link #getRandomElement(List)}, and more.
+ * Utility methods for working with {@link Collection}s. This class contains methods such as {@link
+ * #getRandomElement(List)}, and more.
  *
  * @since 0.1
  */
 public final class CollectionUtils {
 
-    private CollectionUtils() {
-    }
-    
-    /**
-     * Gets a random element in a {@link List}.
-     *
-     * @param list list to use
-     * @return the random element
-     */
-    public static <T> T getRandomElement(List<T> list) {
-        return list.get(NumberUtils.getRandom().nextInt(list.size()));
-    }
+  private CollectionUtils() {
+  }
+
+  /**
+   * Gets a random element in a {@link List}.
+   *
+   * @param list list to use
+   *
+   * @return the random element
+   */
+  public static <T> T getRandomElement(List<T> list) {
+    return list.get(NumberUtils.getRandom().nextInt(list.size()));
+  }
+
+  /**
+   * Gets a random element in a {@link Collection}.
+   *
+   * @param collection collection to use
+   *
+   * @return the random element
+   */
+  public static <T> T getRandomElement(Collection<T> collection) {
+    return ((T) getRandomElement(collection.toArray()));
+  }
+
+  /**
+   * @see ArrayUtils#getRandomElement(Object[])
+   */
+  public static <T> T getRandomElement(T[] array) {
+    return ArrayUtils.getRandomElement(array);
+  }
 }
