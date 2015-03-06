b1a7bd40db8c3899a3a5c1aa21ab5792c8c8c29c
Ali Moghnieh <SupaHam.me@gmail.com>
2015-03-06 10:10:44 +0000

    Added some guava implementations to EntityUtils.

--- commons-bukkit/src/main/java/com/supaham/commons/bukkit/utils/EntityUtils.java
+++ commons-bukkit/src/main/java/com/supaham/commons/bukkit/utils/EntityUtils.java
@@ -2,9 +2,17 @@ package com.supaham.commons.bukkit.utils;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import com.google.common.base.Supplier;
+
+import org.bukkit.Chunk;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 
 import javax.annotation.Nonnull;
 
@@ -28,5 +36,94 @@ public class EntityUtils {
   public static Location getFreeLocation(@Nonnull LivingEntity entity) {
     checkNotNull(entity, "entity cannot be null.");
     return LocationUtils.getFreeLocation(entity.getLocation(), entity.getEyeHeight(true));
+  }
+  
+  /* ================================
+   * >> GUAVA implementations
+   * ================================ */
+
+  /**
+   * Gets a new {@link EntitiesSupplier} which returns all entities in a {@link Chunk}.
+   *
+   * @param chunk chunk to get entities from
+   *
+   * @return entities supplier
+   *
+   * @see #multiChunkEntities(Collection)
+   */
+  public static EntitiesSupplier singleChunkEntities(@Nonnull Chunk chunk) {
+    return multiChunkEntities(chunk);
+  }
+
+  /**
+   * Gets a new {@link EntitiesSupplier} which returns all entities in an array of {@link Chunk}s.
+   *
+   * @param chunks chunks to get entities from
+   *
+   * @return entities supplier
+   *
+   * @see #multiChunkEntities(Collection)
+   */
+  public static EntitiesSupplier multiChunkEntities(@Nonnull Chunk... chunks) {
+    return new ChunkEntitiesSupplier(Arrays.asList(chunks));
+  }
+
+  /**
+   * Gets a new {@link EntitiesSupplier} which returns all entities in a collection of {@link
+   * Chunk}s.
+   *
+   * @param chunks chunks to get entities from
+   *
+   * @return entities supplier
+   */
+  public static EntitiesSupplier multiChunkEntities(@Nonnull Collection<Chunk> chunks) {
+    return new ChunkEntitiesSupplier(chunks);
+  }
+
+  /**
+   * Gets a new {@link EntitiesSupplier} which returns all entities in a {@link World}.
+   *
+   * @param world world to get entities from
+   *
+   * @return entities supplier
+   */
+  public static EntitiesSupplier worldEntities(@Nonnull World world) {
+    return new WorldEntitiesSupplier(world);
+  }
+
+  public static interface EntitySupplier extends Supplier<Entity> {}
+
+  public static interface EntitiesSupplier extends Supplier<Collection<Entity>> {}
+
+  private static class ChunkEntitiesSupplier implements EntitiesSupplier {
+
+    private final Collection<Chunk> chunks;
+
+    public ChunkEntitiesSupplier(Collection<Chunk> chunks) {
+      this.chunks = checkNotNull(chunks, "chunk cannot be null.");
+    }
+
+    @Override
+    public Collection<Entity> get() {
+      ArrayList<Entity> entities = new ArrayList<>();
+      for (Chunk chunk : chunks) {
+        entities.addAll(Arrays.asList(chunk.getEntities()));
+      }
+      return entities;
+    }
+  }
+
+  private static class WorldEntitiesSupplier implements EntitiesSupplier {
+
+    private final World world;
+
+    public WorldEntitiesSupplier(World world) {
+      this.world = checkNotNull(world, "world cannot be null.");
+    }
+
+    @Override
+    public Collection<Entity> get() {
+      return world.getEntities();
+    }
   }
 }
