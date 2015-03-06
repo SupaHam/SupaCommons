4fdbf6999ded241d6e48e932a719ed7908816c5a
Ali Moghnieh <SupaHam.me@gmail.com>
2015-03-06 07:50:15 +0000

    Created DurationSerializer class in commons-core.

---
+++ commons-core/src/main/java/com/supaham/commons/serializers/DurationSerializer.java
@@ -0,0 +1,35 @@
+package com.supaham.commons.serializers;
+
+import com.supaham.commons.utils.DurationUtils;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.joda.time.Duration;
+
+import pluginbase.config.serializers.Serializer;
+
+/**
+ * A {@link Duration} serializer that simply calls {@link DurationUtils#toString(Duration,
+ * boolean)}, with boolean as true, in return.
+ */
+public class DurationSerializer implements Serializer<Duration> {
+
+  @Override
+  @Nullable
+  public Object serialize(@Nullable final Duration duration) {
+    if (duration == null) {
+      return null;
+    }
+    return DurationUtils.toString(duration, true);
+  }
+
+  @Override
+  @Nullable
+  public Duration deserialize(@Nullable Object serialized, @NotNull Class wantedType)
+      throws IllegalArgumentException {
+    if (serialized == null || !(serialized instanceof String)) {
+      return null;
+    }
+    return DurationUtils.parseDuration(serialized.toString());
+  }
+}
