0772b718a262eaa26fbd8cabed498baa5d8a54a7
Ali Moghnieh <SupaHam.me@gmail.com>
2015-03-06 10:20:45 +0000

    Created CommonBukkitException class in commons-bukkit.

---
+++ commons-bukkit/src/main/java/com/supaham/commons/bukkit/CommonBukkitException.java
@@ -0,0 +1,28 @@
+package com.supaham.commons.bukkit;
+
+import com.supaham.commons.CommonException;
+import com.supaham.commons.bukkit.language.Message;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Represents a commons bukkit library exception that can be thrown at any time.
+ */
+public class CommonBukkitException extends CommonException {
+
+  public CommonBukkitException(String message) {
+    super(message);
+  }
+
+  public CommonBukkitException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public CommonBukkitException(Throwable cause) {
+    super(cause);
+  }
+
+  public CommonBukkitException(@Nonnull Message message, Object... args) {
+    super(message.getParsedMessage(args));
+  }
+}
