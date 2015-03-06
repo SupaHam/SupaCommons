f5d1745b844d5f25718c3a4d3b370edb4ecc6078
Ali Moghnieh <SupaHam.me@gmail.com>
2015-03-06 07:58:18 +0000

    Created ColorStringSerializer class in commons-bukkit.

---
+++ commons-bukkit/src/main/java/com/supaham/commons/bukkit/serializers/ColorStringSerializer.java
@@ -0,0 +1,33 @@
+package com.supaham.commons.bukkit.serializers;
+
+import org.bukkit.ChatColor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import pluginbase.config.serializers.Serializer;
+
+/**
+ * A {@link String} {@link Serializer} that specifically serializes minecraft color codes as
+ * ampersand (&) rather than section sign (&sect;).
+ */
+public class ColorStringSerializer implements Serializer<String> {
+
+  @Override
+  @Nullable
+  public Object serialize(@Nullable final String string) {
+    if (string == null) {
+      return null;
+    }
+    return string.replaceAll(ChatColor.COLOR_CHAR + "", "&");
+  }
+
+  @Override
+  @Nullable
+  public String deserialize(@Nullable Object serialized, @NotNull Class wantedType)
+      throws IllegalArgumentException {
+    if (serialized == null || !(serialized instanceof String)) {
+      return null;
+    }
+    return ChatColor.translateAlternateColorCodes('&', serialized.toString());
+  }
+}
