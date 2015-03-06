2545a5bb4b69fc7614a12ec3426d85d214aa57d6
Ali Moghnieh <SupaHam.me@gmail.com>
2015-03-06 08:33:59 +0000

    Created Sound and SingleSound classes in commons-bukkit.

---
+++ commons-bukkit/src/main/java/com/supaham/commons/bukkit/SingleSound.java
@@ -0,0 +1,90 @@
+package com.supaham.commons.bukkit;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import com.supaham.commons.bukkit.utils.OBCUtils;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Represents a single sound with a volume and pitch.
+ */
+public class SingleSound extends Sound {
+
+  private String sound;
+  private float volume;
+  private float pitch;
+
+  /**
+   * Constructs a single sound with a volume of 1 and pitch of 1.
+   *
+   * @param sound sound name to play
+   *
+   * @see #SingleSound(String, float, float)
+   * @see OBCUtils#getSound(org.bukkit.Sound)
+   */
+  public SingleSound(@Nonnull String sound) {
+    this(sound, 1f, 1f);
+  }
+
+  /**
+   * Constructs a single sound.
+   *
+   * @param sound sound name to play
+   * @param volume volume to play the sound at
+   * @param pitch pitch to play the sound at
+   *
+   * @see OBCUtils#getSound(org.bukkit.Sound)
+   */
+  public SingleSound(@Nonnull String sound, float volume, float pitch) {
+    checkNotNull(sound, "sound cannot be null.");
+    this.sound = sound;
+    this.volume = volume;
+    this.pitch = pitch;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof SingleSound)) {
+      return false;
+    }
+    SingleSound other = (SingleSound) obj;
+    return sound.equals(other.sound) && volume == other.volume && pitch == other.pitch;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  @SuppressWarnings("deprecation")
+  public void play(@Nonnull Player player, @Nonnull Location location) {
+    player.playSound(location, this.sound, this.volume, this.pitch);
+  }
+
+  public String getSound() {
+    return sound;
+  }
+
+  public void setSound(String sound) {
+    this.sound = sound;
+  }
+
+  public float getVolume() {
+    return volume;
+  }
+
+  public void setVolume(float volume) {
+    this.volume = volume;
+  }
+
+  public float getPitch() {
+    return pitch;
+  }
+
+  public void setPitch(float pitch) {
+    this.pitch = pitch;
+  }
+}
---
+++ commons-bukkit/src/main/java/com/supaham/commons/bukkit/Sound.java
@@ -0,0 +1,92 @@
+package com.supaham.commons.bukkit;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+import java.util.Collection;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Represents a sound that is playable to a {@link World} or {@link Player}s.
+ */
+public abstract class Sound {
+
+  /**
+   * Plays this sound in a {@link World} at each players' location.
+   *
+   * @param world world to play this sound in
+   * @see #play(Collection)
+   */
+  public void play(@Nonnull World world) {
+    checkNotNull(world, "world cannot be null.");
+    play(world.getPlayers());
+  }
+
+  /**
+   * Plays this sound in a {@link World} at a {@link Location}.
+   *
+   * @param world world to play this sound in
+   * @param location location to play this sound at
+   *
+   * @see #play(Collection, Location)
+   */
+  public void play(@Nonnull World world, @Nonnull Location location) {
+    checkNotNull(world, "world cannot be null.");
+    checkNotNull(location, "location cannot be null.");
+    play(world.getPlayers(), location);
+  }
+
+  /**
+   * Plays this sound to a {@link Collection} of {@link Player}s at their {@link Location}.
+   *
+   * @param players players to play this sound for
+   *
+   * @see #play(Player)
+   */
+  public void play(@Nonnull Collection<Player> players) {
+    checkNotNull(players, "players collection cannot be null.");
+    for (Player player : players) {
+      play(player);
+    }
+  }
+
+  /**
+   * Plays this sound to a {@link Collection} of {@link Player}s.
+   *
+   * @param players players to play this sound for
+   * @param location location to play this sound at
+   *
+   * @see #play(Player, Location)
+   */
+  public void play(@Nonnull Collection<Player> players, @Nonnull Location location) {
+    checkNotNull(players, "players collection cannot be null.");
+    checkNotNull(location, "location cannot be null.");
+    for (Player player : players) {
+      play(player, location);
+    }
+  }
+
+  /**
+   * Plays this sound to a {@link Player} at their location.
+   *
+   * @param player player to play this sound for
+   *
+   * @see #play(Player, Location)
+   */
+  public void play(@Nonnull Player player) {
+    checkNotNull(player, "player cannot be null.");
+    play(player, player.getLocation());
+  }
+
+  /**
+   * Plays this sound to a {@link Player} at a {@link Location}.
+   *
+   * @param player player to play this sound for
+   * @param location location to play this sound at
+   */
+  public abstract void play(@Nonnull Player player, @Nonnull Location location);
+}
