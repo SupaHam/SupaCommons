6c6bda839b0c8c2f99d2238a57518efdf15c2385
Ali Moghnieh <SupaHam.me@gmail.com>
2015-03-17 16:59:50 +0000

    Created Version class in commons-core.

---
+++ commons-core/src/main/java/com/supaham/commons/Version.java
@@ -0,0 +1,14 @@
+package com.supaham.commons;
+
+/**
+ * Represents a class that is versioned.
+ */
+public interface Version {
+
+  /**
+   * Gets the version of this object.
+   *
+   * @return version of this object
+   */
+  String getVersion();
+}
