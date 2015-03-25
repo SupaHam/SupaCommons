7496403210b87de51892776d953afb17ce0fe1b4
Ali Moghnieh <SupaHam.me@gmail.com>
2015-03-25 01:20:11 +0000

    Register all serializers in commons-bukkit in CBSerializers.

--- commons-bukkit/src/main/java/com/supaham/commons/bukkit/serializers/CBSerializers.java
+++ commons-bukkit/src/main/java/com/supaham/commons/bukkit/serializers/CBSerializers.java
@@ -1,11 +1,14 @@
 package com.supaham.commons.bukkit.serializers;
 
+import com.supaham.commons.bukkit.items.ItemEnchantment;
 import com.supaham.commons.serializers.ListSerializer;
 
 import org.bukkit.Location;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.material.MaterialData;
 import org.bukkit.util.Vector;
 
+import pluginbase.config.SerializationRegistrar;
 import pluginbase.config.serializers.Serializer;
 
 /**
@@ -15,37 +18,62 @@ import pluginbase.config.serializers.Serializer;
  * @since 0.1
  */
 public class CBSerializers {
+  
+  static {
+    SerializationRegistrar.registerClass(ColorStringSerializer.class);
+    SerializationRegistrar.registerClass(ComplexItemStackSerializer.class);
+    SerializationRegistrar.registerClass(ItemEnchantmentSerializer.class);
+    SerializationRegistrar.registerClass(LocationSerializer.class);
+    SerializationRegistrar.registerClass(MaterialDataSerializer.class);
+    SerializationRegistrar.registerClass(VectorSerializer.class);
+    
+    SerializationRegistrar.registerClass(ListColorStringSerializer.class);
+    SerializationRegistrar.registerClass(ListComplexItemStackSerializer.class);
+    SerializationRegistrar.registerClass(ListItemEnchantmentSerializer.class);
+    SerializationRegistrar.registerClass(ListLocationSerializer.class);
+    SerializationRegistrar.registerClass(ListMaterialDataSerializer.class);
+    SerializationRegistrar.registerClass(ListVectorSerializer.class);
+  }
 
   public static class ListColorStringSerializer extends ListSerializer<String> {
 
-    @Override
-    public Class<ColorStringSerializer> getSerializerClass() {
+    @Override public Class<ColorStringSerializer> getSerializerClass() {
       return ColorStringSerializer.class;
+    }
+  }
+
+  public static class ListComplexItemStackSerializer extends ListSerializer<ItemStack> {
+
+    @Override public Class<ComplexItemStackSerializer> getSerializerClass() {
+      return ComplexItemStackSerializer.class;
+    }
+  }
+
+  public static class ListItemEnchantmentSerializer extends ListSerializer<ItemEnchantment> {
+
+    @Override public Class<ItemEnchantmentSerializer> getSerializerClass() {
+      return ItemEnchantmentSerializer.class;
     }
   }
   
   public static class ListLocationSerializer extends ListSerializer<Location> {
 
-    @Override
-    public Class<LocationSerializer> getSerializerClass() {
+    @Override public Class<LocationSerializer> getSerializerClass() {
       return LocationSerializer.class;
     }
   }
 
   public static class ListMaterialDataSerializer extends ListSerializer<MaterialData> {
 
-    @Override
-    public Class<MaterialDataSerializer> getSerializerClass() {
+    @Override public Class<MaterialDataSerializer> getSerializerClass() {
       return MaterialDataSerializer.class;
     }
   }
 
   public static class ListVectorSerializer extends ListSerializer<Vector> {
 
-    @Override
-    public Class<VectorSerializer> getSerializerClass() {
+    @Override public Class<VectorSerializer> getSerializerClass() {
       return VectorSerializer.class;
     }
   }
-
 }
