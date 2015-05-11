a25a5d2ed4cb8e7cab0b19be0d8d4fa11938db44
Ali Moghnieh <SupaHam.me@gmail.com>
2015-05-11 16:20:38 +0100

    Added a bunch more utility methods to ReflectionUtils.

--- commons-bukkit/src/main/java/com/supaham/commons/bukkit/utils/ReflectionUtils.java
+++ commons-bukkit/src/main/java/com/supaham/commons/bukkit/utils/ReflectionUtils.java
@@ -1,17 +1,101 @@
 package com.supaham.commons.bukkit.utils;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Utility methods for working with {@code java.reflect}. This class contains methods such as
- * {@link #getServerVersion()}, and more, as well as the {@link PackageType} class for version packages such {@code nms} and {@code obc}.
+ * {@link #getServerVersion()}, and more, as well as the {@link PackageType} class for version
+ * packages such {@code nms} and {@code obc}.
  *
  * @since 0.1
  */
 public class ReflectionUtils {
 
+  private static final Map<String, Class> classes = new HashMap<>();
+  private static final Map<String, Method> methods = new HashMap<>();
+  private static final Map<String, Field> fields = new HashMap<>();
+
+  static {
+    PackageType nms = PackageType.MINECRAFT_SERVER;
+    PackageType obc = PackageType.CRAFTBUKKIT;
+
+    classes.put("entityPlayer", nms.getClassSafe("EntityPlayer"));
+    fields.put("playerConnection", getField(classes.get("entityPlayer"), "playerConnection"));
+    methods.put("sendPacket", getMethod(fields.get("playerConnection").getClass(), "sendPacket"));
+  }
+
+  public static void sendPacket(Player player, Object object) {
+    try {
+      Object connection = fields.get("playerConnection").get(player);
+      methods.get("sendPacket").invoke(connection, object);
+    } catch (IllegalAccessException | InvocationTargetException e) {
+      e.printStackTrace();
+    }
+  }
+
   public static String getServerVersion() {
     return Bukkit.getServer().getClass().getPackage().getName().substring(23);
+  }
+
+  public static Class<?> getNMSClass(String className) {
+    return PackageType.MINECRAFT_SERVER.getClassSafe(className);
+  }
+
+  public static Class<?> getOBCClass(String className) {
+    return PackageType.CRAFTBUKKIT.getClassSafe(className);
+  }
+
+  public static Object getHandle(Object obj) {
+    try {
+      return getMethod(obj.getClass(), "getHandle").invoke(obj);
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  public static Field getField(Class<?> clazz, String name) {
+    try {
+      Field field = clazz.getDeclaredField(name);
+      field.setAccessible(true);
+      return field;
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  public static Method getMethod(Class<?> clazz, String name,
+                                 Class<?>... args) {
+    for (Method m : clazz.getMethods()) {
+      if (m.getName().equals(name) && (args.length == 0 || ClassListEqual(args,
+                                                                          m.getParameterTypes()))) {
+        m.setAccessible(true);
+        return m;
+      }
+    }
+    return null;
+  }
+
+  public static boolean ClassListEqual(Class<?>[] l1, Class<?>[] l2) {
+    boolean equal = true;
+    if (l1.length != l2.length) {
+      return false;
+    }
+    for (int i = 0; i < l1.length; i++) {
+      if (l1[i] != l2[i]) {
+        equal = false;
+        break;
+      }
+    }
+    return equal;
   }
 
   /**
@@ -29,7 +113,7 @@ public class ReflectionUtils {
 
     private final String path;
 
-    private PackageType(String path) {
+    PackageType(String path) {
       this.path = path;
     }
 
@@ -40,6 +124,15 @@ public class ReflectionUtils {
 
     public Class<?> getClass(String className) throws ClassNotFoundException {
       return Class.forName(this + "." + className);
+    }
+
+    public Class<?> getClassSafe(String className) {
+      try {
+        return Class.forName(this + "." + className);
+      } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+        return null;
+      }
     }
 
     public String getPath() {
