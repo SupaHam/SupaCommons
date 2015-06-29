9037259cfa04560c3e395cee5bcca1edabe8d4cb
Ali Moghnieh <SupaHam.me@gmail.com>
2015-06-29 15:45:36 +0100

    Create isAir(ItemStack) method in ItemUtils.

--- commons-bukkit/src/main/java/com/supaham/commons/bukkit/utils/ItemUtils.java
+++ commons-bukkit/src/main/java/com/supaham/commons/bukkit/utils/ItemUtils.java
@@ -1,19 +1,20 @@
 package com.supaham.commons.bukkit.utils;
 
 import org.apache.commons.lang.StringUtils;
+import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nullable;
 
 /**
- * Utility methods for working with {@link ItemStack} instances. This class contains methods such as
- * {@link #getEnchantment(String)}, and more.
+ * Utility methods for working with {@link ItemStack} instances. This class contains methods such
+ * as {@link #getEnchantment(String)}, {@link #isAir(ItemStack)}, and more.
  *
  * @since 0.1
  */
 public class ItemUtils {
-  
+
   /**
    * Gets an {@link Enchantment} by {@link String}. This method check if the given string is
    * numeric, if it is, {@link Enchantment#getById(int)} is called with the string parsed as
@@ -27,5 +28,17 @@ public class ItemUtils {
   public static Enchantment getEnchantment(String string) {
     return StringUtils.isNumeric(string) ? Enchantment.getById(Integer.parseInt(string))
                                          : Enchantment.getByName(string);
+  }
+
+  /**
+   * Returns whether an {@link ItemStack} is equivalent to {@link Material#AIR}. If the given
+   * {@code item} is null, true is returned, meaning the given item is air.
+   *
+   * @param item itemstack to check
+   *
+   * @return whether the {@code item} is air
+   */
+  public static boolean isAir(@Nullable ItemStack item) {
+    return item == null || item.getType() == Material.AIR;
   }
 }
