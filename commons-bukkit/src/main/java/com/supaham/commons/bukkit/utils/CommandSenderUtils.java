aac15df76a5d1c596d0463fed88ff4e35a1c9c3d
Ali Moghnieh <SupaHam.me@gmail.com>
2015-07-07 00:36:08 +0100

    Create CommandSenderUtils#getDisplayName(CommandSender).

--- commons-bukkit/src/main/java/com/supaham/commons/bukkit/utils/CommandSenderUtils.java
+++ commons-bukkit/src/main/java/com/supaham/commons/bukkit/utils/CommandSenderUtils.java
@@ -11,8 +11,8 @@ import org.bukkit.entity.Player;
  * @since 0.1
  */
 public final class CommandSenderUtils {
-
-  private CommandSenderUtils() {}
+  
+  public static final String CONSOLE_NAME = "CONSOLE";
 
   /**
    * Gets the name of a {@link CommandSender}. If the sender is a {@link Player}, their display
@@ -23,12 +23,31 @@ public final class CommandSenderUtils {
    *
    * @return {@code sender}'s name
    */
-  public static String getName(CommandSender sender) {
+  public static String getDisplayName(CommandSender sender) {
     if (sender instanceof Player) {
       return ((Player) sender).getDisplayName();
     } else if (sender instanceof ConsoleCommandSender) {
-      return "CONSOLE";
+      return CONSOLE_NAME;
     }
     return sender.getName();
   }
+  /**
+   * Gets the name of a {@link CommandSender}. If the sender is a {@link Player}, their
+   * name is returned. If the the sender is the {@link ConsoleCommandSender}, {@link #CONSOLE_NAME} 
+   * is returned. Otherwise, {@link CommandSender#getName()} is returned.
+   *
+   * @param sender sender to get name for
+   *
+   * @return {@code sender}'s name
+   */
+  public static String getName(CommandSender sender) {
+    if (sender instanceof Player) {
+      return sender.getName();
+    } else if (sender instanceof ConsoleCommandSender) {
+      return CONSOLE_NAME;
+    }
+    return sender.getName();
+  }
+
+  private CommandSenderUtils() {}
 }
