3dd6bc6dd7c6810ed773092ebca84fcd30e9eec2
Ali Moghnieh <SupaHam.me@gmail.com>
2015-07-19 22:37:04 +0100

    Comment out broken test.

--- commons-bukkit/src/test/java/com/supaham/commons/bukkit/utils/MNCSBannerGeneratorTest.java
+++ commons-bukkit/src/test/java/com/supaham/commons/bukkit/utils/MNCSBannerGeneratorTest.java
@@ -3,14 +3,13 @@ package com.supaham.commons.bukkit.utils;
 import com.supaham.commons.bukkit.utils.misc.MNCSBannerGenerator;
 
 import org.bukkit.inventory.ItemStack;
-import org.junit.Test;
 
 /**
  * Created by Ali on 12/07/2015.
  */
 public class MNCSBannerGeneratorTest {
 
-  @Test
+  // @Test can't test
   public void testName() throws Exception {
     ItemStack itemStack = MNCSBannerGenerator
         .parseURL("http://www.needcoolshoes.com/banner?=naauhufufkmkkwoCglnnpjit");
