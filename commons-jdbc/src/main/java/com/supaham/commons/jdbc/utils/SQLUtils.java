29d2e8f5d269369abc6e1df42a53ba681ae698b9
Ali Moghnieh <SupaHam.me@gmail.com>
2015-09-17 23:37:03 +0100

    Create SQLUtils#hasColumn(ResultSet, String).

--- commons-jdbc/src/main/java/com/supaham/commons/jdbc/utils/SQLUtils.java
+++ commons-jdbc/src/main/java/com/supaham/commons/jdbc/utils/SQLUtils.java
@@ -2,8 +2,11 @@ package com.supaham.commons.jdbc.utils;
 
 import static com.supaham.commons.utils.StringUtils.checkNotNullOrEmpty;
 
+import com.google.common.base.Preconditions;
+
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
 import javax.annotation.Nonnull;
@@ -18,7 +21,7 @@ import javax.sql.DataSource;
 public class SQLUtils {
 
   /**
-   * Checks if a {@link DataSource}'s {@link Connection} to a database has a table. This method 
+   * Checks if a {@link DataSource}'s {@link Connection} to a database has a table. This method
    * calls {@link #hasTable(Connection, String)} and passes it {@link DataSource#getConnection()},
    * it also handles closing the given {@link Connection}.
    *
@@ -39,7 +42,7 @@ public class SQLUtils {
   }
 
   /**
-   * Checks if a {@link Connection} has a table. This method does not close the given 
+   * Checks if a {@link Connection} has a table. This method does not close the given
    * {@link Connection}. Please refer to {@link #hasTable(DataSource, String)} for an auto close.
    *
    * @param connection connection to use
@@ -51,6 +54,30 @@ public class SQLUtils {
     checkNotNullOrEmpty(namePattern, "namePattern");
     try (ResultSet rs = connection.getMetaData().getTables(null, null, namePattern, null)) {
       return rs.next();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return false;
+  }
+
+  /**
+   * Returns whether a {@link ResultSet} contains a column by label.
+   *
+   * @param resultSet result set to check
+   * @param columnLabel label to check
+   *
+   * @return whether the {@code resultSet} contains the {@code columnLabel}
+   */
+  public static boolean hasColumn(@Nonnull ResultSet resultSet, @Nonnull String columnLabel) {
+    Preconditions.checkNotNull(resultSet, "result set cannot be null.");
+    Preconditions.checkNotNull(columnLabel, "column label cannot be null.");
+    try {
+      ResultSetMetaData md = resultSet.getMetaData();
+      for (int i = 1; i < md.getColumnCount() + 1; i++) {
+        if (md.getColumnLabel(i).equalsIgnoreCase(columnLabel)) {
+          return true;
+        }
+      }
     } catch (SQLException e) {
       e.printStackTrace();
     }
