8aa4480df37eaa336caef74bf104d948873ba7da
Ali Moghnieh <SupaHam.me@gmail.com>
2016-01-05 05:40:15 +0000

    Switch from Gradle to Maven to support the server-side community more.

--- commons-core/src/test/java/com/supaham/commons/DurationUtilsTest.java
+++ commons-core/src/test/java/com/supaham/commons/DurationUtilsTest.java
@@ -4,9 +4,10 @@ import static com.supaham.commons.utils.DurationUtils.parseDuration;
 
 import com.supaham.commons.utils.DurationUtils;
 
-import org.joda.time.Duration;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.time.Duration;
 
 /**
  * Created by Ali on 09/02/2015.
@@ -16,23 +17,23 @@ public class DurationUtilsTest {
   @Test
   public void testParse() throws Exception {
     Duration duration = parseDuration("1h");
-    Assert.assertEquals(3600, duration.getStandardSeconds());
+    Assert.assertEquals(3600, duration.getSeconds());
   }
 
   @Test
   public void testToString() throws Exception {
-    Assert.assertEquals("1h", DurationUtils.toString(Duration.standardSeconds(3600), true));
-    Assert.assertEquals("1 hour", DurationUtils.toString(Duration.standardSeconds(3600), false));
+    Assert.assertEquals("1h", DurationUtils.toString(Duration.ofSeconds(3600), true));
+    Assert.assertEquals("1 hour", DurationUtils.toString(Duration.ofSeconds(3600), false));
   }
 
   @Test
   public void testRandomDuration() throws Exception {
-    Duration d1 = new Duration(0);
-    Duration d2 = new Duration(1000);
+    Duration d1 = Duration.ofMillis(0);
+    Duration d2 = Duration.ofMillis(1000);
     for (int i = 0; i < 10000; i++) {
       Duration randomDuration = DurationUtils.randomDuration(d1, d2);
-      Assert.assertTrue(randomDuration.getMillis() >= d1.getMillis()
-                        && randomDuration.getMillis() <= d2.getMillis());  
+      Assert.assertTrue(randomDuration.toMillis() >= d1.toMillis()
+                        && randomDuration.toMillis() <= d2.toMillis());  
     }
   }
 }
