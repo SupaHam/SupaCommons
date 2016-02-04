1b70608e18560b8cb4760171e8b3952cf23f95f6
Ali Moghnieh <SupaHam.me@gmail.com>
2016-02-04 17:01:42 +0000

    Create CommonCollectors#singleRandom() for collecting single random elements from streams.

--- commons-core/src/main/java/com/supaham/commons/CommonCollectors.java
+++ commons-core/src/main/java/com/supaham/commons/CommonCollectors.java
@@ -2,9 +2,13 @@ package com.supaham.commons;
 
 import com.google.common.collect.Lists;
 
+import com.supaham.commons.utils.CollectionUtils;
+
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collector;
+import java.util.stream.Stream;
 
 public class CommonCollectors {
 
@@ -13,7 +17,7 @@ public class CommonCollectors {
    * {@link ArrayList}.
    * <p/>
    * Thanks to <a href="http://stackoverflow.com/questions/24010109/java-8-stream-reverse-order">
-   *   http://stackoverflow.com/questions/24010109/java-8-stream-reverse-order</a>
+   * http://stackoverflow.com/questions/24010109/java-8-stream-reverse-order</a>
    *
    * @param <T> type of list to return
    *
@@ -24,6 +28,22 @@ public class CommonCollectors {
       l.addAll(r);
       return l;
     }, Lists::<T>reverse);
+  }
+
+  /**
+   * Creates a {@link Collector} random element from a {@link Stream}. This method collects the stream and calls
+   * {@link CollectionUtils#getRandomElement(Collection)} which supplies the final result.
+   *
+   * @param <T> type of object to return
+   *
+   * @return random element
+   */
+  public static <T> Collector<T, List<T>, T> singleRandom() {
+    // TODO make this more efficient by not having to create a list and add.
+    return Collector.of(ArrayList::new, List::add, (l, r) -> {
+      l.addAll(r);
+      return l;
+    }, CollectionUtils::getRandomElement);
   }
 
   private CommonCollectors() {
