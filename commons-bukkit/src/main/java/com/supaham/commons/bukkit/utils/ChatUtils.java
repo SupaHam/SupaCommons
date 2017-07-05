4ede998895ee741a111ba9ee1e2c5091096b7fb8
Ali Moghnieh <supaham@supaham.com>
2017-07-05 03:27:42 +0100

    Add a bunch of methods to ChatUtils.

--- commons-bukkit/src/main/java/com/supaham/commons/bukkit/utils/ChatUtils.java
+++ commons-bukkit/src/main/java/com/supaham/commons/bukkit/utils/ChatUtils.java
@@ -1,16 +1,26 @@
 package com.supaham.commons.bukkit.utils;
 
+import com.supaham.commons.Enums;
 import com.supaham.commons.bukkit.utils.ReflectionUtils.PackageType;
 
 import net.kyori.text.Component;
+import net.kyori.text.KeybindComponent;
+import net.kyori.text.SelectorComponent;
+import net.kyori.text.TextComponent;
+import net.kyori.text.format.TextColor;
+import net.kyori.text.format.TextDecoration;
 import net.kyori.text.serializer.ComponentSerializer;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class ChatUtils {
 
@@ -36,14 +46,85 @@ public class ChatUtils {
       e.printStackTrace();
     }
   }
-
-  public static void sendComponent(Player player, Component component) {
-    sendComponent(Collections.singleton(player), component);
+  
+  public static <T extends Component> T forceResetStyles(T component) {
+    component = (T) component.color(null);
+    for (TextDecoration textDecoration : TextDecoration.values()) {
+      component = (T) component.decoration(textDecoration, false);
+    }
+    return component;
+  }
+  
+  public static String getReadableComponent(Component component) {
+    StringBuilder sb = new StringBuilder();
+    getReadableComponentSingle(sb, component);
+    for (Component child : component.children()) {
+      getReadableComponentSingle(sb, child);
+    }
+    return sb.toString();
   }
 
-  public static void sendComponent(Iterable<Player> players, Component component) {
-    String json = ComponentSerializer.serialize(component);
-    sendJson(players, json);
+  public static void getReadableComponentSingle(StringBuilder sb, Component component) {
+    sb.append(textColorToBukkit(component.color()));
+    if (component instanceof TextComponent) {
+      sb.append(((TextComponent) component).content());
+    } else if (component instanceof KeybindComponent) {
+      sb.append(((KeybindComponent) component).keybind());
+    } else if (component instanceof SelectorComponent) {
+      sb.append(((SelectorComponent) component).pattern());
+    } else {
+      throw new UnsupportedOperationException("Cannot parse " + component.getClass());
+    }
+  }
+  
+  public static ChatColor textColorToBukkit(TextColor textColor) {
+    return Enums.findByValue(ChatColor.class, textColor.name());
+  }
+  
+  public static ChatColor textDecorationToBukkit(TextDecoration textDecoration) {
+    if (textDecoration == TextDecoration.OBFUSCATED) {
+      return ChatColor.MAGIC;
+    }
+    return Enums.findByValue(ChatColor.class, textDecoration.name());
+  }
+
+  public static void sendComponent(CommandSender sender, Component component) {
+    if (sender instanceof Player) {
+      String json = ComponentSerializer.serialize(component);
+      sendJson(Collections.singleton(((Player) sender)), json);
+    } else {
+      sendStringComponent(sender, component);
+    }
+  }
+
+  public static void sendComponent(Iterable<CommandSender> senders, Component component) {
+    List<Player> players = new ArrayList<>();
+    List<CommandSender> others = new ArrayList<>();
+    for (CommandSender sender : senders) {
+      if (sender instanceof Player) {
+        players.add(((Player) sender));
+      } else {
+        others.add(sender);
+      }
+    }
+    if (!players.isEmpty()) {
+      String json = ComponentSerializer.serialize(component);
+      sendJson(players, json);
+    }
+    if (!others.isEmpty()) {
+      sendStringComponent(others, component);
+    }
+  }
+
+  public static void sendStringComponent(CommandSender sender, Component component) {
+    sendStringComponent(Collections.singleton(sender), component);
+  }
+
+  public static void sendStringComponent(Iterable<CommandSender> senders, Component component) {
+    String str = getReadableComponent(component);
+    for (CommandSender sender : senders) {
+      sender.sendMessage(str);
+    }
   }
 
   public static void sendJson(Player player, String json) {
@@ -60,7 +141,7 @@ public class ChatUtils {
       e.printStackTrace();
     }
   }
-  
+
   public static Object nmsFromComponent(Component component) {
     return nmsFromJson(ComponentSerializer.serialize(component));
   }
